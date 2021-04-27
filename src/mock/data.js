edb796042661d651413c6755732929e10146537a
Gabe Valdivia <gabe@gabeswebcoding.com>
2021-04-26 22:44:33 -0500

    Changed hero subtitle

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,7 +11,7 @@ export const headData = {
 export const heroData = {
   title: 'Web Developer',
   name: 'Gabriel Valdivia',
-  subtitle: 'WordPress, WooCommerce, Genesis',
+  subtitle: 'WordPress, WooCommerce, UI',
   cta: 'Available!',
 };
 
